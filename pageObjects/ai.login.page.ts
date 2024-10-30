8585120b691ea5888096126b4d21a79167c39eea
damarre <damare.ananta@gmail.com>
2024-10-30 10:25:08 +0700

    rename func

--- pageObjects/ai.login.page.ts
+++ pageObjects/ai.login.page.ts
@@ -1,17 +1,18 @@
 /**
  * author: damar
- * date  : 4/10/2024
+ * date  : 30/10/2024
  */
+
 import fs from 'fs';
-import { ais, chatGPT, savePageSource, getCommandFromChatGPT } from '../helper/gptHelper';
+import { ai, chatGPT, savePageSource, getCommandFromChatGPT } from '../helper/gptHelper';
 
 
 class LoginPage {
 
   async doLogin() {
-    await ais(`User want to fill "username" field with "standard_user"`);
-    await ais(`User want to fill "password" field with "secret_sauce"`);
-    await ais(`User click "LOGIN" button`);
+    await ai(`User want to fill "username" field with "standard_user"`);
+    await ai(`User want to fill "password" field with "secret_sauce"`);
+    await ai(`User click "LOGIN" button`);
   }
 
   async selectProduct() {
